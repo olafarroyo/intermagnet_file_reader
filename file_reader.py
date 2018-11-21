a588d1b87e0a6d9deadd705a0a00ba510f10e46a
olaf <olaf.c.arroyo@gmail.com>
2018-11-21 14:27:22 +0100

    se incluye archivo intermagnet en gitignore

--- file_reader.py
+++ file_reader.py
@@ -1,1 +1,14 @@
-import numpy as np
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 21 14:01:00 2018
+
+@author: olaf
+"""
+
+a=open('tuc20181021vsec.sec','r')
+b=a.read()
+
+c=b.split('\n')
+
+print(c[0:10])
